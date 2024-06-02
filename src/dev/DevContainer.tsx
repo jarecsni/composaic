2974423053226352c22bfe48deab0bfe177c1966
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2024-06-02 11:07:17 +0100

    🚧 Added a call to the empty DevContainer from within the plugin project

---
+++ src/dev/DevContainer.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+const SimpleComponent: FC = () => {
+    return (
+        <div>
+            <h1>Hello, world!</h1>
+        </div>
+    );
+};
+
+export default SimpleComponent;
--- test-apps/plugin-template/vite.config.ts
+++ test-apps/plugin-template/vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import alias from '@rollup/plugin-alias'
+// import federation from '@originjs/vite-plugin-federation'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        alias({
+            entries: [
+                {
+                    find: '@composaic',
+                    replacement: resolve(__dirname, '../../src'),
+                },
+                {
+                    find: '@',
+                    replacement: resolve(__dirname, './src'),
+                },
+            ],
+        }),
+    ],
 })
