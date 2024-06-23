9bb9d959909890588adc48ea01b364c15809ed1f
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2024-06-23 15:10:18 +0100

    🚧 Logger plugin implementation

--- src/plugins/impl/logger/index.ts
+++ src/plugins/impl/logger/index.ts
@@ -1,6 +1,11 @@
 import { Plugin } from '../../types';
 
-export interface LoggerExtension {
+/**
+ * Logger extension point.
+ * 
+ * Extensions for this extension point will need to implement these methods. 
+ */
+export interface LoggerExtensionPoint {
     log(message: string, ...args: unknown[]): void;
     info(message: string, ...args: unknown[]): void;
     debug(message: string, ...args: unknown[]): void;
@@ -8,4 +13,24 @@ export interface LoggerExtension {
     error(message: string, ...args: unknown[]): void;
 }
 
-export class LoggerPlugin extends Plugin {}
+export class LoggerPlugin extends Plugin {
+
+}
+
+export class SimpleLoggerExtension implements LoggerExtensionPoint {
+    log(message: string, ...args: unknown[]): void {
+        console.log(message, ...args);
+    }
+    info(message: string, ...args: unknown[]): void {
+        console.log(message, ...args);
+    }
+    debug(message: string, ...args: unknown[]): void {
+        console.debug(message, ...args);
+    }
+    warn(message: string, ...args: unknown[]): void {
+        console.warn(message, ...args);
+    }
+    error(message: string, ...args: unknown[]): void {
+        console.error(message, ...args);
+    }
+}
