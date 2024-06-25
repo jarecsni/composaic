3d811135af9f2e5d2ca4a580936235ef8dcfedd7
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2024-06-25 06:55:58 +0100

    🚧 Remote test for bundle inclusion

--- test-apps/vite/remote-a/src/App.tsx
+++ test-apps/vite/remote-a/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
+import { sayHello } from './util/say-hello';
 
 function App() {
     return (
         <>
             <h1>This is Remote A!</h1>
+            <p>{sayHello()}</p>
         </>
     )
 }
