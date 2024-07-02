c110c484e2637947f430a87cf12569bc514e1e45
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2024-07-02 07:00:23 +0100

    🚧 Working on passing manifest and module to devcontainer

--- src/dev/DevContainer.tsx
+++ src/dev/DevContainer.tsx
@@ -1,11 +1,18 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { PluginManager } from '../plugins/PluginManager';
 // @ts-expect-error - this is not working in VScode
 import corePlugins from '../plugins/core-plugins.json';
+import { PluginManifest } from '../plugins/types';
 
 PluginManager.getInstance().addPluginDefinitions(corePlugins);
 
-const SimpleComponent: FC = () => {
+interface DevContainerProps {
+    manifest: PluginManifest;
+    loadModule(moduleName: string, pkg: string): Promise<object>;
+}
+
+export const DevContainer: FC<DevContainerProps> = ({ manifest, loadModule }) => {
+    console.log('DevContainer', JSON.stringify(manifest));
     return (
         <div>
             <h1>Hello, world!</h1>
@@ -13,4 +20,3 @@ const SimpleComponent: FC = () => {
     );
 };
 
-export default SimpleComponent;
--- test-apps/plugin-template/src/App.tsx
+++ test-apps/plugin-template/src/App.tsx
@@ -1,9 +1,15 @@
 import './App.css';
 // @ts-expect-error - resolution not working
-import DevContainer from '@composaic/dev/DevContainer';
+import { DevContainer } from '@composaic/dev/DevContainer';
+import manifest from '../manifest.json';
+
+const loadModule = async (moduleName: string, pkg: string) => {
+    const module = await import(`./plugins/${pkg}/${moduleName}.ts`);
+    return module;
+}
 
 function App() {
-    return <DevContainer></DevContainer>;
+    return <DevContainer manifest={manifest} loadModule={loadModule} ></DevContainer >;
 }
 
 export default App;
