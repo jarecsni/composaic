9f10fb0430c4901af5219d0b35e8ef3d64542b41
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2024-07-05 15:46:53 +0100

    🔥 Remove commented out stuff

--- test-apps/plugin-template/src/App.tsx
+++ test-apps/plugin-template/src/App.tsx
@@ -1,20 +1,6 @@
-import { useEffect, useState } from 'react';
 import './App.css';
 // @ts-expect-error - resolution not working
 import { DevContainer } from '@composaic/dev/DevContainer';
-//import manifest from '../manifest.json' with { type: 'json' };
-
-//console.log('manifest', manifest);
-
-// import('../manifest.json').then((manifest) => {
-//     console.log('manifest', manifest);
-// });
-
-// import('../manifest.json')
-//     .then((json) => {
-//         console.log('manifest', json.default);
-//     })
-//     .catch((error) => console.error('Failed to load manifest:', error));
 
 const loadModule = async (moduleName: string, pkg: string) => {
     const module = await import(`./plugins/${pkg}/${moduleName}.ts`);
@@ -22,16 +8,7 @@ const loadModule = async (moduleName: string, pkg: string) => {
 };
 
 function App() {
-    // const [manifest, setManifest] = useState({});
-    // useEffect(() => {
-    //     fetch('/manifest.json').then((response) => {
-    //         response.json().then((json) => {
-    //             console.log('setting manifest');
-    //             setManifest(json);
-    //         })
-    //     });
-    // }, []);
-    return <DevContainer loadModule={loadModule}></DevContainer>;
+    return <DevContainer loadModule={loadModule} ></DevContainer >;
 }
 
 export default App;
