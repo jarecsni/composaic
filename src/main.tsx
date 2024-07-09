c098f98cc4cb5ecf61a489b940561073329b4035
jarecsni <jarecsni@users.noreply.github.com>
2024-07-09 06:05:37 +0000

    fix styling

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,16 @@ import { ConfigurationService } from './services/configuration';
 import './index.css';
 
 console.log('Env:', process.env.NODE_ENV);
-ConfigurationService.getInstance().getConfiguration().remotes.forEach(async (remote) => {
-    console.log('Remote:', remote);
-    const manifest = await fetch(remote + '/manifest.json');
-    console.log('Manifest:', await manifest.json());
-    const script = await fetch('http://localhost:9000/TestPlugins.js');
-    const scriptText = await script.text();
-    console.log('Script:', scriptText);
-});
-
+ConfigurationService.getInstance()
+    .getConfiguration()
+    .remotes.forEach(async (remote) => {
+        console.log('Remote:', remote);
+        const manifest = await fetch(remote + '/manifest.json');
+        console.log('Manifest:', await manifest.json());
+        const script = await fetch('http://localhost:9000/TestPlugins.js');
+        const scriptText = await script.text();
+        console.log('Script:', scriptText);
+    });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
