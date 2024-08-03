6ec200b18cf0901b69fd05dd761fba8629bdc566
jarecsni <jarecsni@users.noreply.github.com>
2024-08-03 14:30:20 +0000

    fix styling

--- test-apps/plugin-template/src/plugins/navbar/NavbarExtension.ts
+++ test-apps/plugin-template/src/plugins/navbar/NavbarExtension.ts
@@ -1,8 +1,7 @@
 import { Plugin } from 'composaic';
 export { RemoteExamplePage } from './RemoteExamplePage';
 
-export class NavbarExtensionPlugin extends Plugin {
-}
+export class NavbarExtensionPlugin extends Plugin {}
 
 export class NavbarItemExtension {
     // TODO remove this as it's not needed
