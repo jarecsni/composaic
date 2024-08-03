ce435058aa78a965477dcb3ac4d2d0f21ed67040
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2024-08-03 15:29:28 +0100

    🔊 Add logging for starting/stopping plugins

--- test-apps/plugin-template/src/plugins/navbar/NavbarExtension.ts
+++ test-apps/plugin-template/src/plugins/navbar/NavbarExtension.ts
@@ -2,14 +2,6 @@ import { Plugin } from 'composaic';
 export { RemoteExamplePage } from './RemoteExamplePage';
 
 export class NavbarExtensionPlugin extends Plugin {
-    async start() {
-        super.start();
-        console.log('NavbarExtensionPlugin started');
-    }
-    async stop() {
-        super.stop();
-        console.log('NavbarExtensionPlugin stopped');
-    }
 }
 
 export class NavbarItemExtension {
