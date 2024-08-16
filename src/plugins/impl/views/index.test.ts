d6354683007a1c34679345d232c71740cb242caf
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2024-08-16 15:23:40 +0100

    Add Views plugin

---
+++ src/plugins/impl/views/index.test.ts
@@ -0,0 +1,45 @@
+import { PluginViewDefinition, ViewDefinition, ViewsPlugin } from '.';
+
+describe('ViewsPlugin', () => {
+    describe('consolidateViews', () => {
+        it('should consolidate plugin view definitions', () => {
+            const pluginViewDefinitions: PluginViewDefinition[] = [
+                {
+                    container: 'container1',
+                    components: ['component1', 'component2'],
+                    plugin: 'plugin1',
+                },
+                {
+                    container: 'container2',
+                    components: ['component3'],
+                    plugin: 'plugin2',
+                },
+                {
+                    container: 'container1',
+                    components: ['component4'],
+                    plugin: 'plugin3',
+                },
+            ];
+
+            const expectedViewDefinitions: ViewDefinition[] = [
+                {
+                    container: 'container1',
+                    components: [
+                        { component: 'component1', plugin: 'plugin1' },
+                        { component: 'component2', plugin: 'plugin1' },
+                        { component: 'component4', plugin: 'plugin3' },
+                    ],
+                },
+                {
+                    container: 'container2',
+                    components: [{ component: 'component3', plugin: 'plugin2' }],
+                },
+            ];
+
+            const viewsPlugin = new ViewsPlugin();
+            const consolidatedViews = viewsPlugin.consolidateViews(pluginViewDefinitions);
+
+            expect(consolidatedViews).toEqual(expectedViewDefinitions);
+        });
+    });
+});
---
+++ src/plugins/impl/views/index.ts
@@ -0,0 +1,81 @@
+import { Plugin } from '../../types';
+
+export type PluginViewDefinition = {
+    container: string;
+    components: string[];
+    plugin: string;
+}
+
+export type ViewDefinition = {
+    container: string;
+    components: {
+        component: string;
+        plugin: string;
+    }[];
+}
+/**
+ * Views extension point.
+ */
+export interface ViewExtensionPoint {
+    getViewDefinitions(): ViewDefinition[];
+}
+
+export class ViewsPlugin extends Plugin {
+    private viewsDefinitons: ViewDefinition[] = [];
+
+    async start() {
+        super.start();
+        // Collect navbar items from all connected extensions
+        const pluginViewsDefinitons: PluginViewDefinition[] = [];
+        this.getConnectedExtensions('views').forEach((extension) => {
+            const navBarMeta = extension.meta! as PluginViewDefinition[];
+            for (const item of navBarMeta) {
+                item.plugin = extension.plugin;
+                pluginViewsDefinitons.push(item);
+            }
+        });
+        this.viewsDefinitons = this.consolidateViews(pluginViewsDefinitons);
+    }
+
+    consolidateViews(pluginViewDefinitions: PluginViewDefinition[]): ViewDefinition[] {
+        const viewDefinitions: ViewDefinition[] = [];
+        pluginViewDefinitions.forEach((pluginViewDefinition) => {
+            const existingViewDefinition = viewDefinitions.find((viewDefinition) => viewDefinition.container === pluginViewDefinition.container);
+            if (existingViewDefinition) {
+                pluginViewDefinition.components.forEach((component) => {
+                    existingViewDefinition.components.push({
+                        component: component,
+                        plugin: pluginViewDefinition.plugin
+                    });
+                });
+            } else {
+                viewDefinitions.push({
+                    container: pluginViewDefinition.container,
+                    components: pluginViewDefinition.components.map((component) => {
+                        return {
+                            component: component,
+                            plugin: pluginViewDefinition.plugin
+                        }
+                    })
+                });
+            }
+        });
+        return viewDefinitions
+    }
+
+    async stop() {
+        // Clear navbar items or any other cleanup
+        this.viewsDefinitons = [];
+    }
+
+    public getViewDefinitions(): ViewDefinition[] {
+        return this.viewsDefinitons;
+    }
+}
+
+// TODO: This is unnecessary as the information is provided by the manifest 
+export class SimpleViewsExtension implements ViewExtensionPoint {
+    getViewDefinitions(): ViewDefinition[] {
+        return [];
+    }
+}
