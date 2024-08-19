34f1b77f97dcee05877085207b656d21bbc37a8c
jarecsni <jarecsni@users.noreply.github.com>
2024-08-19 07:23:48 +0000

    fix styling

--- src/plugins/impl/navbar/Example1Page.tsx
+++ src/plugins/impl/navbar/Example1Page.tsx
@@ -3,7 +3,9 @@ import { PluginManager } from '../../PluginManager';
 import { ViewDefinition, ViewsPlugin } from '../views';
 
 export const Example1Page: React.FC = () => {
-    const [viewDefinition, setViewDefinition] = useState<ViewDefinition | undefined>(undefined);
+    const [viewDefinition, setViewDefinition] = useState<
+        ViewDefinition | undefined
+    >(undefined);
 
     useEffect(() => {
         // Assuming PluginManager has a method getPlugin to get a plugin by name
@@ -11,7 +13,9 @@ export const Example1Page: React.FC = () => {
         const pluginManager = PluginManager.getInstance(); // Or however you access PluginManager
         pluginManager.getPlugin('@composaic/views').then((viewsPlugin) => {
             if (viewsPlugin) {
-                const containerViewDefinition = (viewsPlugin as ViewsPlugin).getViewsByContainer('sample.container');
+                const containerViewDefinition = (
+                    viewsPlugin as ViewsPlugin
+                ).getViewsByContainer('sample.container');
                 setViewDefinition(containerViewDefinition);
             }
         });
@@ -21,14 +25,15 @@ export const Example1Page: React.FC = () => {
         <div>
             Example1Page
             <div>
-                {viewDefinition && viewDefinition.components.map((component) => {
-                    return (
-                        <div key={component.plugin}>
-                            <div>Plugin: {component.plugin}</div>
-                            <div>Component: {component.component}</div>
-                        </div>
-                    );
-                })}
+                {viewDefinition &&
+                    viewDefinition.components.map((component) => {
+                        return (
+                            <div key={component.plugin}>
+                                <div>Plugin: {component.plugin}</div>
+                                <div>Component: {component.component}</div>
+                            </div>
+                        );
+                    })}
             </div>
         </div>
     );
