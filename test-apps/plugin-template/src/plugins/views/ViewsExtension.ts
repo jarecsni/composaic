0531a7d6241a37d37ffb6a037d74fe403c381438
jarecsni <jarecsni@users.noreply.github.com>
2024-08-20 05:45:13 +0000

    fix styling

--- test-apps/plugin-template/src/plugins/views/ViewsExtension.ts
+++ test-apps/plugin-template/src/plugins/views/ViewsExtension.ts
@@ -1,11 +1,7 @@
-import { Plugin } from 'composaic'
+import { Plugin } from 'composaic';
 // @ts-expect-error this not working in VSC
 import { ViewsExtensionPoint } from '@composaic/plugins/views';
 
-export class ViewsExtensionsPlugin extends Plugin {
+export class ViewsExtensionsPlugin extends Plugin {}
 
-}
-
-export class SimpleViewsExtension implements ViewsExtensionPoint {
-
-}
+export class SimpleViewsExtension implements ViewsExtensionPoint {}
