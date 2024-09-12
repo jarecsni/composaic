602ded3f0110ae6723b4fbd050e80b078ffc718a
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2024-09-12 23:47:23 +0100

    👷 Removed dead vite code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-// IMPORTANT DO NOT ADD DevContainer IMPORTS HERE!
-// Nothing should be exposed here which eventually hits vite's __federation__ module
-
 export { Plugin } from './plugins/types.js';
 export { LoggerExtensionPoint, LogMessage } from './plugins/impl/logger';
