7e9d7e8e3d29d3c7f23224bffe03403f3183b3a1
Janos Jarecsni <janos.jarecsni@gmail.com>
2024-10-01 14:35:36 +0100

    🔨 Fix symlink script

--- scripts/symlink.sh
+++ scripts/symlink.sh
@@ -1,8 +1,15 @@
 cd ../node_modules
 echo "Creating symlinks for composaic-demo"
 echo "Current folder: $(pwd)"
+echo "erasing folders"
+rm -rf react
+rm -rf react-dom
+rm -rf react-router
+rm -rf react-router-dom
+echo "Creating symlinks"
 ln -s ../../composaic-demo/node_modules/react react
 ln -s ../../composaic-demo/node_modules/react-dom react-dom
 ln -s ../../composaic-demo/node_modules/react-router react-router
 ln -s ../../composaic-demo/node_modules/react-router-dom react-router-dom
 cd ..
+echo "Symlinks created"
